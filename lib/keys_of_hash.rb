09d785323119619d240e457d99e746047e00b352
Juan Alorro <jralorro93@gmail.com>
2019-02-14 02:57:21 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,9 +4,10 @@ class Hash
   def keys_of(*arguments)
     new_array = []
     self.each do |key, value|
-              binding.pry
-      if value == arguments
-        new_array << key 
+      value.each do |country|
+        if country == arguments
+          new_array << key 
+        end 
       end 
     end 
     new_array
